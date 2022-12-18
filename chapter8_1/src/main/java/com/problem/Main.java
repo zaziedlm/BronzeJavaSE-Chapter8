0952fa84cbc4fbab67f9693c65fff78e715475e2
Shuu Kataoka <shu01k9@gmail.com>
2022-12-18 20:51:12 +0900

    initialCommit

---
+++ chapter8_1/src/main/java/com/problem/Main.java
@@ -0,0 +1,18 @@
+package com.problem;
+
+/**
+ * Chapter8_1
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        for(int i = 0; i < 5; i++) {
+            for(i = 5; i < 10; i++) {
+                // System.out.println(i);
+                System.out.print(i);
+            }
+        }
+    }
+}
