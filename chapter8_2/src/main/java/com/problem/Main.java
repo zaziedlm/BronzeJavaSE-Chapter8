a544e6155e05f68b79af53268899363b0b1b4ac1
Shuichi Kataoka <shu01k9@gmail.com>
2023-01-02 11:04:27 +0900

    package changed.

---
+++ chapter8_2/src/main/java/com/problem/Main.java
@@ -0,0 +1,12 @@
+package com.problem;
+
+/**
+ * Chpter8_2
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println((i += 5) + ":" + (i--));
+    }
+}
